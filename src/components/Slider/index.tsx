c5fe54b147a59f82d6537d8ef18a18f949bf1779
guilherme.domingos <guilherme.domingos@gamifica.ai>
2021-12-22 08:24:16 -0300

    clinapse1

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,27 +1,35 @@
-import { useRef } from "react";
-import SlickSlider from "react-slick";
+import { useRef, useState, useEffect } from 'react'
+import SlickSlider from 'react-slick'
 
-import { Container, Carousel, TextArea, ImageArea, ButtonArea } from "./style";
-import LinkButton from "../LinkButton";
-
+import {
+  Container,
+  Carousel,
+  TextArea,
+  ImageArea,
+  ButtonArea,
+  SmContainer
+} from './style'
+import LinkButton from '../LinkButton'
+import ShowMore from 'react-show-more-button'
 
 interface ISlider {
   currentSlide: {
-    position: number;
-    name: string;
-    media?: string;
-  };
+    position: number
+    name: string
+    media?: string
+  }
 }
 
 export default function Slider({ currentSlide }: ISlider) {
-  const sliderRef = useRef<SlickSlider>(null);
-  const phoneNumber = "5561981223602";  
+  const sliderRef = useRef<SlickSlider>(null)
+  const phoneNumber = '5561981223602'
+  const [showMore, setShowMore] = useState(false)
 
   if (currentSlide) {
-    sliderRef.current?.slickGoTo(currentSlide.position);
+    sliderRef.current?.slickGoTo(currentSlide.position)
 
     if (currentSlide.media) {
-      sliderRef.current?.slickPause();
+      sliderRef.current?.slickPause()
     }
   }
 
@@ -31,143 +39,120 @@ export default function Slider({ currentSlide }: ISlider) {
     speed: 2500,
     slidesToShow: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
-    slidesToScroll: 1,
-  };
+    autoplaySpeed: 30000,
+    slidesToScroll: 1
+  }
+
+  const extraContent1 = (
+    <div>
+      <p>
+        A partir dos resultados desses testes, é possível
+        identificar/compreender as funções cerebrais preservadas e comprometidas
+        do idoso e definir o melhor tratamento para buscar a estabilização ou
+        até mesmo a redução de déficits neuropsicológicos apresentados em cada
+        caso.
+      </p>
+    </div>
+  )
   return (
     <Container>
       <Carousel ref={sliderRef} {...settings}>
         <div>
           <TextArea>
-            <h1>Diagnóstico Neuropsicológico para Idosos</h1>
-            <span>Profissionais presentes no seu cotidiano</span>
+            <h1>Avaliação Neuropsicológico para Idosos</h1>
+            <span>
+              Trata-se de um procedimento que utiliza testes cognitivos que
+              possuem o objetivo de identificar alterações comportamentais e
+              estabelecer o perfil neuropsicológico de cada paciente.
+            </span>
             <p>
-              é uma prática clínica desenvolvida fora dos espaços tradicionais
-              de tratamento (consultórios) e que acontece no contexto de vida de
-              quem é acompanhado, utilizando o cotidiano como território
-              privilegiado para suas experiências e intervenções.
+              A partir dos resultados desses testes, é possível
+              identificar/compreender as funções cerebrais preservadas e
+              comprometidas do idoso e definir o melhor tratamento para buscar a
+              estabilização ou até mesmo a redução de déficits neuropsicológicos
+              apresentados em cada caso.
             </p>
+            
+          <ButtonArea>
+            <LinkButton
+              title="Agende sua consulta"
+              href={`https://api.whatsapp.com/send?phone=${phoneNumber}&text=Olá, quero agendar uma consulta`}
+            />
+          </ButtonArea>
           </TextArea>
 
-          <ButtonArea> 
-
-
-          <LinkButton
-            title="Agende sua consulta"
-            href={`https://api.whatsapp.com/send?phone=${phoneNumber}&text=Olá, quero agendar uma consulta`}
-          />
-          
-
-
-          </ButtonArea>
-
-          <ImageArea>
-            <img
-              className="main"
-              src="https://images.unsplash.com/photo-1637176473226-4772830471b5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1528&q=80"
-              alt="Photo1"
-            />
-          </ImageArea>
+        </div>
+        
+        <div>
+          <TextArea>
+            <h1>Reabilitação Neurocognitiva</h1>
+            <span>
+              Consiste em um tratamento não medicamentoso, em que são oferecidas
+              oficinas direcionadas para cada paciente e sua família, com a
+              intenção de facilitar a compreensão das dificuldades cognitivas,
+              comportamentais ou emocionais apresentadas pelo idoso.
+            </span>
+            <p>
+              O procedimento inclui oficinas de atenção, memória, cálculo e
+              linguagem em um conjunto de técnicas personalizadas para cada
+              paciente de acordo com sua necessidade. Dessa forma, a
+              reabilitação neurocognitiva apresenta-se como uma estratégia
+              cientificamente comprovada e busca recuperar a autonomia, a
+              independência e a facilitação do convívio com o paciente ao longo
+              do tratamento.
+            </p>
+            <ButtonArea>
+              <LinkButton
+                title="Agende sua consulta"
+                href={`https://api.whatsapp.com/send?phone=${phoneNumber}&text=Olá, quero agendar uma consulta`}
+              />
+            </ButtonArea>
+          </TextArea>
         </div>
         <div>
           <TextArea>
             <h1>Psicologia Clínica</h1>
-            <span>Profissionais presentes no seu cotidiano</span>
+            <span>
+              Método conduzido por um(a) psicólogo(a) que tem como foco a
+              promoção da saúde mental em um ambiente seguro e acolhedor.
+            </span>
             <p>
-              é uma prática clínica desenvolvida fora dos espaços tradicionais
-              de tratamento (consultórios) e que acontece no contexto de vida de
-              quem é acompanhado, utilizando o cotidiano como território
-              privilegiado para suas experiências e intervenções.
+              Por meio da psicoterapia, é possível alcançar o alívio do
+              sofrimento psíquico, de angústias, medos, traumas e outros
+              construtos que estejam impedindo seu bem-estar. Este serviço é
+              oferecido para adultos e idosos e pode ser realizado
+              individualmente ou em grupo.
             </p>
-          </TextArea>
-          <ButtonArea> 
-
-
-          <LinkButton
-            title="Agende sua consulta"
-            href={`https://api.whatsapp.com/send?phone=${phoneNumber}&text=Olá, quero agendar uma consulta`}
-          />
-          </ButtonArea>
-
-          <ImageArea>
-            
-            <img
-              className="third"
-              src="https://images.unsplash.com/photo-1637176473226-4772830471b5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1528&q=80"
-              alt="Photo3"
+          <ButtonArea>
+            <LinkButton
+              title="Agende sua consulta"
+              href={`https://api.whatsapp.com/send?phone=${phoneNumber}&text=Olá, quero agendar uma consulta`}
             />
-          </ImageArea>
-        </div>
-        <div>
-          <TextArea>
-            <h1>Reabilitação Cognitiva</h1>
-            <span>Profissionais presentes no seu cotidiano</span>
-            <p>
-              é uma prática clínica desenvolvida fora dos espaços tradicionais
-              de tratamento (consultórios) e que acontece no contexto de vida de
-              quem é acompanhado, utilizando o cotidiano como território
-              privilegiado para suas experiências e intervenções.
-            </p>
-          </TextArea>
-
-          
-          <ButtonArea> 
-
-
-          <LinkButton
-            title="Agende sua consulta"
-            href={`https://api.whatsapp.com/send?phone=${phoneNumber}&text=Olá, quero agendar uma consulta`}
-          />
           </ButtonArea>
-
-
-
-          <ImageArea>
-            <img
-              className="main"
-              src="https://images.unsplash.com/photo-1637176473226-4772830471b5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1528&q=80"
-              alt="Photo1"
-            />
-            
-          </ImageArea>
+          </TextArea>
         </div>
         <div>
           <TextArea>
             <h1>Acompanhamento terapêutico</h1>
-            <span>Profissionais presentes no seu cotidiano</span>
+            <span>
+              Trata-se de uma psicoterapia realizada fora do espaço clínico, ou
+              seja, em um ambiente que o paciente se sinta confortável.
+            </span>
             <p>
-              é uma prática clínica desenvolvida fora dos espaços tradicionais
-              de tratamento (consultórios) e que acontece no contexto de vida de
-              quem é acompanhado, utilizando o cotidiano como território
-              privilegiado para suas experiências e intervenções.
+            A proposta do Acompanhamento Terapêutico é de atender ao paciente fora da clínica,  o atendimento acontece no seu cotidiano: em casa, na rua, na padaria, no shopping,  no mercado, cursos... conforme a necessidade do sujeito.  
+            O foco está em promover a autonomia que foi perdida em algum aspecto ou em evitar essa perda. A fim de ampliar os recursos internos e externos na tentativa de potencializar, resgatar e facilitar o processo de reinserção do sujeito dentro do contexto familiar, social ou profissional.  De maneira que a sua singularidade possa comparecer e que este venha a circular de forma mais saudável.
+            A indicação é feita conforme planejamento terapêutico avaliado.
             </p>
+          <ButtonArea>
+            <LinkButton
+              title="Agende sua consulta"
+              href={`https://api.whatsapp.com/send?phone=${phoneNumber}&text=Olá, quero agendar uma consulta`}
+            />
+          </ButtonArea>
           </TextArea>
 
-           
-
-          <ButtonArea> 
-
-
-          <LinkButton
-            title="Agende sua consulta"
-            href={`https://api.whatsapp.com/send?phone=${phoneNumber}&text=Olá, quero agendar uma consulta`}
-          />
-          
-
-
-          </ButtonArea>
-          <ImageArea>
-            <img
-              className="third"
-              src="https://images.unsplash.com/photo-1637176473226-4772830471b5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1528&q=80"
-              alt="Photo3"
-            />
-          </ImageArea>
         </div>
       </Carousel>
     </Container>
-  );
+  )
 }
-
-          
-  
